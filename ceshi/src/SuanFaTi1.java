87812d44ac30fa4fb0d6ab36ff762fd515c170b1
hyomineunjung <623462260@qq.com>
2019-06-19 20:18:25 +0800

    第一次上传作业的修改

--- ceshi/src/SuanFaTi1.java
+++ ceshi/src/SuanFaTi1.java
@@ -4,9 +4,9 @@ public class SuanFaTi1 {
     public static void main(String[] args){
         Scanner sr= new Scanner(System.in);
         int num=sr.nextInt();
-        int count=0;
+        int count=1;
 
-        for (int i=1;num>=1;i++){
+        for (int i=1;num>=10;i++){
             num=num/10;
             count++;
         }System.out.println(count);
